4e772e26391d480f751e54143e83f3e58216b841
bmangir <berkantmangir23@gmail.com>
2026-01-12 13:01:06 +0300

    change: window to limit func, add: safe divide func, caches, fix: unused var(now), join dfs, make:readable, reusable

--- spark/batch/daily_trends.py
+++ spark/batch/daily_trends.py
@@ -4,6 +4,29 @@ from pyspark.sql import functions as F
 import config
 from spark import utility
 
+# Col names to fill NA values with 0
+NUMERIC_ZERO_COLS = [
+    "total_views",
+    "total_likes",
+    "total_dislikes",
+    "total_comments",
+    "total_completes",
+    "unique_viewers",
+    "yesterday_total_views",
+    "yesterday_total_likes",
+]
+
+# Weights of features to calc trend score
+WEIGHTS = {
+    "total_views": 0.10,
+    "total_likes": 0.15,
+    "completion_rate": 0.30,
+    "engagement_rate": 0.15,
+    "avg_watch_percentage": 0.10,
+    "views_velocity": 0.10,
+    "likes_velocity": 0.10,
+}
+
 def run_pipeline(spark):
     # Fetch data
     comments_df, likes_df, watch_history_df = fetch_data(spark)
@@ -48,6 +71,28 @@ def prepare_features(
 
     now = F.current_timestamp()
 
+    comments_df.cache()
+    likes_df.cache()
+    watch_history_df.cache()
+
+    # Get at least one video id that had taken one action last 48 hours.
+    base_videos_df = (
+        watch_history_df
+        .filter(F.col("last_watched_at") >= now - F.expr("INTERVAL 48 HOURS"))
+        .select("video_id")
+        .union(
+            likes_df
+            .filter(F.col("created_at") >= now - F.expr("INTERVAL 48 HOURS"))
+            .select("video_id")
+        )
+        .union(
+            comments_df
+            .filter(F.col("updated_at") >= now - F.expr("INTERVAL 48 HOURS"))
+            .select("video_id")
+        )
+        .distinct()
+    )
+
     yesterday_likes_df = (likes_df.filter(
         (F.col("created_at") >= now - F.expr("INTERVAL 48 HOURS")) &
         (F.col("created_at") <  now - F.expr("INTERVAL 24 HOURS"))
@@ -60,13 +105,13 @@ def prepare_features(
     ).groupBy("video_id").agg(F.coalesce(F.count("*"), F.lit(0)).alias("yesterday_total_views")).select("video_id", "yesterday_total_views")
 
     comments_agg = (comments_df
-                   .filter((F.col("updated_at") >= F.current_timestamp() - F.expr("INTERVAL 24 HOURS")) & (F.col("updated_at") <  now))
+                   .filter((F.col("updated_at") >= now - F.expr("INTERVAL 24 HOURS")) & (F.col("updated_at") <  now))
                    .groupBy("video_id")
                    .agg(F.coalesce(F.count("*"), F.lit(0)).alias("total_comments"))
                    ).select("video_id", "total_comments")
 
     likes_agg = (likes_df
-                 .filter((F.col("created_at") >= F.current_timestamp() - F.expr("INTERVAL 24 HOURS")) & (F.col("created_at") <  now))
+                 .filter((F.col("created_at") >= now - F.expr("INTERVAL 24 HOURS")) & (F.col("created_at") <  now))
                  .groupBy("video_id")
                  .agg(
                     F.sum(F.when(F.col("like_type")==1, 1).otherwise(0)).alias("total_likes"),
@@ -74,7 +119,7 @@ def prepare_features(
                  ).select("video_id", "total_likes", "total_dislikes")
 
     watch_history_agg = (watch_history_df
-                         .filter((F.col("last_watched_at") >= F.current_timestamp() - F.expr("INTERVAL 24 HOURS")) & (F.col("last_watched_at") < now))
+                         .filter((F.col("last_watched_at") >= now - F.expr("INTERVAL 24 HOURS")) & (F.col("last_watched_at") < now))
                          .groupBy("video_id")
                          .agg(
                             F.count("*").alias("total_views"),
@@ -84,53 +129,56 @@ def prepare_features(
                             )
                          ).select("video_id", "total_views", "total_completes", "unique_viewers", "avg_watch_percentage")
 
-    feats_df = ((watch_history_agg
-                .join(comments_agg, "video_id", "left")
-                .join(likes_agg, "video_id", "left")
-                .join(yesterday_likes_df, "video_id", "left")
-                .join(yesterday_watch_df, "video_id", "left")
-                 )
-                .fillna({
-                        "total_views": 0,
-                        "total_likes": 0,
-                        "yesterday_total_views": 0,
-                        "yesterday_total_likes": 0,
-                        "total_dislikes": 0,
-                        "total_comments": 0,
-                        "total_completes": 0,
-                        "unique_viewers": 0,
-                        "avg_watch_percentage": 0.0
-                })
-                .withColumn("engagement_rate", F.when(F.col("total_views") != 0, (F.col("total_likes") + F.col("total_comments")) / F.col("total_views")).otherwise(0.0))
-                .withColumn("completion_rate", F.when(F.col("total_views") != 0, F.col("total_completes") / F.col("total_views")).otherwise(0.0))
-                .withColumn("views_velocity", F.when(F.col("yesterday_total_views") != 0, (F.col("total_views") - F.col("yesterday_total_views")) / F.col("yesterday_total_views")).otherwise(0.0))
-                .withColumn("likes_velocity", F.when(F.col("yesterday_total_likes") != 0, (F.col("total_likes") - F.col("yesterday_total_likes")) / F.col("yesterday_total_likes")).otherwise(0.0))
-                .withColumn("daily_trend_score",
-                            0.10*F.col("total_views") +
-                            0.15*F.col("total_likes") +
-                            0.30*F.col("completion_rate") +
-                            0.15*F.col("engagement_rate") +
-                            0.10*F.col("avg_watch_percentage") +
-                            0.10*F.col("views_velocity") +
-                            0.10*F.col("likes_velocity")
-                            )
-                ).select(
-        "video_id", "total_views", "total_likes", "total_dislikes", "total_comments", "total_completes", "unique_viewers",
-        "avg_watch_percentage", "completion_rate", "engagement_rate", "views_velocity", "likes_velocity", "daily_trend_score"
+    joined_df = (
+        base_videos_df
+        .join(watch_history_agg, "video_id", "left")
+        .join(yesterday_watch_df, "video_id", "left")
+        .join(comments_agg, "video_id", "left")
+        .join(likes_agg, "video_id", "left")
+        .join(yesterday_likes_df, "video_id", "left")
     )
 
-    return feats_df
+    joined_df = joined_df.fillna(0, subset=NUMERIC_ZERO_COLS)
+
+    joined_df = joined_df.fillna({
+        "avg_watch_percentage": 0.0
+    })
+
+    feats_df = joined_df.withColumns({
+        "engagement_rate": safe_div(
+            F.col("total_likes") + F.col("total_comments"),
+            F.col("total_views")
+        ),
+        "completion_rate": safe_div(
+            F.col("total_completes"),
+            F.col("total_views")
+        ),
+        "views_velocity": safe_div(
+            F.col("total_views") - F.col("yesterday_total_views"),
+            F.col("yesterday_total_views")
+        ),
+        "likes_velocity": safe_div(
+            F.col("total_likes") - F.col("yesterday_total_likes"),
+            F.col("yesterday_total_likes")
+        ),
+    })
+
+    trend_score_expr = sum(
+        F.col(col) * F.lit(weight)
+        for col, weight in WEIGHTS.items()
+    )
+
+    feats_df = feats_df.withColumn("daily_trend_score", trend_score_expr)
+
+    return feats_df.select(
+        "video_id", "total_views", "total_likes", "total_dislikes", "total_comments", "total_completes", "unique_viewers",
+        "avg_watch_percentage", "completion_rate", "engagement_rate", "views_velocity", "likes_velocity", "daily_trend_score")
 
 def top_N_trends(df:DataFrame, N=50) -> DataFrame:
-    window = Window.partitionBy("video_id").orderBy("daily_trend_score")
-    df = (df
-          .withColumn(
-            "rnk",
-                      F.row_number().over(window))
-          .filter(F.col("rnk") <= N).drop("rnk")
-    )
+    return df.orderBy(F.col("daily_trend_score").desc()).limit(N)
 
-    return df
+def safe_div(num, denom):
+    return F.when(denom != 0, num / denom).otherwise(F.lit(0.0))
 
 if __name__ == "__main__":
     spark = utility.create_spark_session("Daily_Recommendation_Job")
